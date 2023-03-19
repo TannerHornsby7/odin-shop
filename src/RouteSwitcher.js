877db467f275f902ad461eb1478a5f7167c8db6e
Tanner Hornsby <thornsby@uchicago.edu>
2023-03-18 20:44:52 -0500

    single page render functioning

--- src/RouteSwitcher.js
+++ src/RouteSwitcher.js
@@ -1,4 +1,5 @@
 import {HashRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
 
 
 export default function RouteSwitcher() {
@@ -6,7 +7,7 @@ export default function RouteSwitcher() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} /> */}
         {/* <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/NotFound" element={<NotFound />} /> */}
