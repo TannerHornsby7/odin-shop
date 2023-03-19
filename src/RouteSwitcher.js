4855a7869617a1a843e2236b8c3fcf304144261a
Tanner Hornsby <thornsby@uchicago.edu>
2023-03-18 20:43:32 -0500

    finish initial set up

---
+++ src/RouteSwitcher.js
@@ -0,0 +1,16 @@
+import {HashRouter, Routes, Route} from 'react-router-dom';
+
+
+export default function RouteSwitcher() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/NotFound" element={<NotFound />} /> */}
+      </Routes>
+    </HashRouter>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+// create a basic react homepage with a link to the about page
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      {/* <Link to="/about">About</Link> */}
+    </div>
+  );
+}
